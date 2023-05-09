d996e7353ef234987bc0523147f5d67acf00d27a
Omar Ochoa <omar@ochoawebdesign.com>
2023-05-08 22:08:36 -0700

    PHP Output

---
+++ 01_output.php
@@ -0,0 +1,22 @@
+<?php 
+    echo 'Hello Test', "test";
+    echo '<br>';
+    print 'Printed this';
+    echo '<br>';
+    print_r('Yo, I am printed_rd');
+
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1><?php echo "Post One"; ?></h1>
+    </body>
+    </html>
